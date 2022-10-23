4df0f47342d4b3a476aa2b1f749871dc04c88509
Дмитрий Тюрев <dmitry_tyurev@playrix.com>
2022-10-23 13:28:18 +0300

    Doom: редакторинг, чтение bmp

---
+++ Doom/Doom/LevelData.h
@@ -0,0 +1,40 @@
+#include <vector>
+
+struct FPoint2D
+{
+	float x;
+	float z;
+};
+
+struct DPoint2D
+{
+	double x;
+	double z;
+};
+
+struct Edge
+{
+	int firstInd = 0;  // Индекс первой вершины ребра в verts. Индекс второй вершины ребра берём из следующего ребра в edges
+	int adjPolyN = -1;  // Индекс смежного полигона в polies, либо -1, если смежного полигона нет
+	int adjEdgeN = 0;  // Индекс смежного ребра в edges смежного полигона
+	// Текстурные координаты стен
+	float u[2] = {};    // U-координаты в первой и второй точках ребра
+	float vCeil = 0;     // V-координата а уровне потолка
+	float vCeilAdd = 1;  // Приращение V-координаты (в единицах [0..1] на 1 метр сдвига вниз)
+	float vFloor = 0;    // V-координата а уровне пола
+	float vFloorAdd = 1; // Приращение V-координаты (в единицах [0..1] на 1 метр сдвига вниз)
+	// Текстурные координаты пола/потолка
+	float uFloorCeil = 0;
+	float vFloorCeil = 0;
+};
+
+struct Poly
+{
+	float yRoof = 0;
+	float yCeil = 0;
+	float yFloor = 0;
+	std::vector<Edge> edges;
+};
+
+
+void FillLevelData(std::vector<FPoint2D>& verts, std::vector<Poly>& polies);
