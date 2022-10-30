3740a2ab0f229f95338253f2e02b1073a96c201b
Дмитрий Тюрев <dmitry_tyurev@playrix.com>
2022-10-30 23:14:36 +0300

    Рефакторинг

---
+++ Doom/Doom/Level1Room.h
@@ -0,0 +1,3 @@
+#include "LevelData.h"
+
+void FillLevel1Room(std::vector<FPoint2D>& verts, std::vector<Poly>& polies, std::vector<Texture>& textures);
---
+++ Doom/Doom/Level2Corridor.h
@@ -0,0 +1,3 @@
+#include "LevelData.h"
+
+void FillLevel2Corridor(std::vector<FPoint2D>& verts, std::vector<Poly>& polies, std::vector<Texture>& textures);
